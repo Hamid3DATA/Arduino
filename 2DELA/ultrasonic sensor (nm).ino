93c9554e742fd8b53e481d9b15ee24b394886498
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2023-09-01 09:22:56 +0200

    Add files via upload

---
+++ 2DELA/ultrasonic sensor (nm).ino
@@ -0,0 +1,88 @@
+
+//Green, Red Blue
+
+const int trigpin = 6;
+const int echopin = 5;
+const int red = 10;
+const int green = 11;
+const int blue = 9;
+
+long duration;
+int distance;
+
+
+
+int redlight = 254;
+int greenlight = 1;
+int bluelight = 127;
+
+int reddim = -1;
+int greendim = 1;
+int bluedim = -1;
+
+
+
+void setup(){
+
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
+  Serial.begin(9600);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+
+}
+
+void loop(){
+
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin, LOW);
+
+  duration = pulseIn(echopin, HIGH);
+  distance = 0.017 * duration;
+
+
+  analogWrite(red, redlight);
+  analogWrite(green, greenlight);
+  analogWrite(blue, bluelight);
+
+    redlight = redlight + reddim;
+  greenlight = greenlight + greendim;
+  bluelight = bluelight + bluedim;
+
+  
+  if (distance < 5) {
+    digitalWrite(blue, HIGH);
+    digitalWrite(green, LOW);
+    digitalWrite(red, LOW);
+}
+else if (distance <10) {
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, LOW);
+}
+else { distance > 10;
+  if (redlight >= 255 || redlight <=0){
+    reddim = reddim * -1;
+  }
+  
+  if (greenlight >= 255 || greenlight <=0){
+    greendim = greendim * -1;
+  }
+  
+  if (bluelight >= 255 || bluelight <=0){
+    bluedim = bluedim * -1;
+  }
+}
+
+
+Serial.println("distance: ");
+Serial.println(distance);
+Serial.println(" cm");
+
+delay(50);
+
+}
